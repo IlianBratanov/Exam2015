f9d2ed627e2ef615a74da4878b82d5f52a44f613
ilian1902 <ilian.bratanov82@gmail.com>
2015-02-04 09:28:36 +0200

    Add exam

---
+++ 04/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int d = int.Parse(Console.ReadLine());
+        int hight = n * n + 1;
+        int empti = d + 1;
+
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write(new string ('#', i));
+            Console.Write("\\");
+            Console.Write(new string(' ', d));
+            Console.Write('\\');
+            Console.Write(new string('.', i));
+
+            Console.Write("/");
+            Console.Write(new string(' ', d));
+            Console.WriteLine();
+        }
+    }
+}
+
